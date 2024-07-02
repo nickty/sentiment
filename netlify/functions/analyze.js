f8438cfdf8edbc31c3287ca8638d5709e1e5b422
Mizanur Rahman <mizan@arogga.net>
2024-07-02 16:25:43 +0600

    serverless functions

--- netlify/functions/analyze.js
+++ netlify/functions/analyze.js
@@ -3,10 +3,17 @@ const Sentiment = require("sentiment");
 const sentiment = new Sentiment();
 
 exports.handler = async (event, context) => {
-  const { text } = JSON.parse(event.body);
-  const result = sentiment.analyze(text);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-  };
+  try {
+    const { text } = JSON.parse(event.body);
+    const result = sentiment.analyze(text);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid request" }),
+    };
+  }
 };
