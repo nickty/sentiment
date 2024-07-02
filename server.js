5a5e1b08cc0ef2d386c26e35be00cbb8b6f3e0c4
Mizanur Rahman <mizan@arogga.net>
2024-07-02 16:08:57 +0600

    access for all

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const Sentiment = require('sentiment');
-const cors = require('cors');
+const express = require("express");
+const Sentiment = require("sentiment");
+const cors = require("cors");
 
 const app = express();
 const port = 5000;
 
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 
 const sentiment = new Sentiment();
 
-app.post('/analyze', (req, res) => {
+app.post("/analyze", (req, res) => {
   const { text } = req.body;
   const result = sentiment.analyze(text);
   res.json(result);
@@ -19,4 +19,3 @@ app.post('/analyze', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
